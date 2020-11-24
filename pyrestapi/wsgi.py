d4cf17aa21e44f83d2aa79fe82b95b39dbac5752
Nayan Patel <nayan.patel@firstdata.com>
2020-11-24 15:54:25 -0500

    Test

--- pyrestapi/wsgi.py
+++ pyrestapi/wsgi.py
@@ -4,17 +4,13 @@ WSGI config for pyrestapi project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-#import django.core.handlers.wsgi
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyrestapi.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyrestapi.settings")
 
 application = get_wsgi_application()
-
-
-#application = django.core.handlers.wsgi.WSGIHandler()
