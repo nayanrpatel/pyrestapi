0742c4e6a97fa237f6f8a9921fb1dac7db966139
Nayan Patel <nayan.patel@firstdata.com>
2020-11-24 14:10:12 -0500

    Uncommented the code

--- pyrestapi/settings.py
+++ pyrestapi/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'pyrestapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
