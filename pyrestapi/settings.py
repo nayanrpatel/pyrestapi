2bc9952e8b12a049e44faf613354ce54e0fe35ae
Nayan Patel <nayan.patel@firstdata.com>
2020-11-24 14:03:58 -0500

    Uncommented the code

--- pyrestapi/settings.py
+++ pyrestapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ft+sk_*h8r(*nx75rmcu*otcp9!2@ht86=%m@*(fmrqk8umx-_'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
